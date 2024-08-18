75e5c14b39568b086aa530b9a6fece424cc16d4a
Richard Ayikwei <richardayikweibee@gmail.com>
2024-08-18 16:31:35 +0000

    positioning of elements

--- src/components/ProfileInfo.tsx
+++ src/components/ProfileInfo.tsx
@@ -6,17 +6,46 @@ type ProfileProps = {
 
 const ProfileInfo = ({ richard }: ProfileProps) => {
   return (
-    <div className="flex md:flex-row md:justify-between text-[#1f3550] mt-14 ">
-      <div className=" flex flex-col  md:w-1/2 mx-auto">
+    <div className="flex md:flex-row text-[#1f3550] mt-14 2xl:mt-56 md:mx-20 ">
+      <div className=" flex flex-col md:w-1/2 mb-6">
         <div className=" flex flex-row">
-          <img
-            src={richard.profilePic}
-            alt="profilePic"
-            className="rounded-full max-h-96"
-          />
+          <div>
+            <img
+              src={richard.profilePic}
+              alt="profilePic"
+              className="rounded-full max-h-96"
+            />
+          </div>
+
+          <div className=" flex-col flex justify-between md:hidden  ">
+            <a
+              href="https://github.com/richardayikwei"
+              target="blank"
+              className="transition ease-linear duration-500 hover:rotate-45"
+            >
+              <img src={assets.social.githubIcon} alt="" className="w-11" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/richard-ayikwei-48b171112/"
+              target="blank"
+              className="transition ease-linear duration-500 hover:rotate-45"
+            >
+              <img src={assets.social.linkedInIcon} alt="" className="w-11" />
+            </a>
+            <a
+              href=""
+              className="transition ease-linear duration-500 hover:rotate-45"
+            >
+              <img
+                src={assets.social.facebookIcon}
+                alt="facebookIcon"
+                className="w-11"
+              />
+            </a>
+          </div>
         </div>
 
-        <div className="space-y-2">
+        <div className="space-y-2 mt-6">
           <div className="text-3xl">
             <h1>{richard.name}</h1>
           </div>
@@ -26,50 +55,23 @@ const ProfileInfo = ({ richard }: ProfileProps) => {
           <div className="text-xl">
             <p>{richard.motto}</p>
           </div>
-          <div className="text-lg lg:w-1/2 flex">
+          <div className="text-lg lg:w-2/3 flex">
             <p>{richard.cover}</p>
           </div>
 
-          {/* <div className=" hidden justify-start">
-          <img src={richard.stack.html} alt="" className="w-20" />
-          <img src={richard.stack.css} alt="" className="w-20" />
-          <img src={richard.stack.javaScript} alt="" className="w-20" />
-          <img src={richard.stack.jQuery} alt="" className="w-20" />
-          <img src={richard.stack.typeScript} alt="" className="w-20" />
-          <img src={richard.stack.react} alt="" className="w-20" />
-          <img src={richard.stack.tailwindCSS} alt="" className="w-20" />
-        </div> */}
+          <div className=" md:hidden justify-start flex-wrap flex">
+            <img src={richard.stack.html} alt="" className="w-20" />
+            <img src={richard.stack.css} alt="" className="w-20" />
+            <img src={richard.stack.javaScript} alt="" className="w-20" />
+            <img src={richard.stack.jQuery} alt="" className="w-20" />
+            <img src={richard.stack.typeScript} alt="" className="w-20" />
+            <img src={richard.stack.react} alt="" className="w-20" />
+            <img src={richard.stack.tailwindCSS} alt="" className="w-20" />
+          </div>
         </div>
-
-        {/* <div className=" flex-col flex justify-between md:hidden">
-          <a
-            href="https://github.com/richardayikwei"
-            target="blank"
-            className="transition ease-linear duration-500 hover:rotate-45"
-          >
-            <img src={assets.social.githubIcon} alt="" className="w-11" />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/richard-ayikwei-48b171112/"
-            target="blank"
-            className="transition ease-linear duration-500 hover:rotate-45"
-          >
-            <img src={assets.social.linkedInIcon} alt="" className="w-11" />
-          </a>
-          <a
-            href=""
-            className="transition ease-linear duration-500 hover:rotate-45"
-          >
-            <img
-              src={assets.social.facebookIcon}
-              alt="facebookIcon"
-              className="w-11"
-            />
-          </a>
-        </div> */}
       </div>
 
-      <div className=" md:flex md:flex-col hidden md:w-96 items-center justify-between">
+      <div className=" md:flex md:flex-col hidden md:w-1/2 items-end justify-between">
         <img src={richard.stack.html} alt="" className="max-w-20" />
         <img src={richard.stack.css} alt="" className="max-w-20" />
         <img src={richard.stack.javaScript} alt="" className="max-w-20" />
