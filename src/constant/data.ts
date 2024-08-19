a2abfeceaa24ce098bc95943e2b7d252c645e08b
Richard Ayikwei <richardayikweibee@gmail.com>
2024-08-19 01:13:10 +0000

    full stop

--- src/constant/data.ts
+++ src/constant/data.ts
@@ -8,7 +8,7 @@ export const profile: Profile[] = [
         name: 'Ayikwei Richard',
         position: 'Frontend Developer',
         motto: 'Change is constant',
-        cover: 'I am a front-end developer specializing in the React framework. I am passionate about creating visually appealing websites and apps with  easy to navigate UI"s',
+        cover: 'I am a front-end developer specializing in the React framework. I am passionate about creating visually appealing websites and apps with  easy to navigate UI"s.',
         stack: {
             css: assets.stackIcons.cssIcon,
             html: assets.stackIcons.htmlIcon,
