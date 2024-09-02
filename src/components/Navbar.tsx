65550d1e556f497739732d2db1f8a89f25aefe25
Richard Ayikwei <richardayikweibee@gmail.com>
2024-09-02 18:53:03 +0000

    changing a to Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,29 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <header className=" flex md:flex-row flex-col md:justify-end pt-9 container mx-auto w-[90%]">
       <nav className="w-[700px] h-20 md:flex justify-around items-center bg-[#a3c0e2] hidden rounded-3xl">
-        <a
-          href="/"
+        <Link
+          to="/"
           className="text-[#1f3550] text-lg hover:text-white"
         >
           About me
-        </a>
-        <a
-          href="/projects"
+        </Link>
+        <Link
+          to="/projects"
           className="text-[#1f3550] text-lg hover:text-white"
         >
           Projects
-        </a>
-        <a
-          href=""
+        </Link>
+        <Link
+          to=""
           target=""
           rel=""
           className="text-[#1f3550] text-lg hover:text-white"
         >
           Contact me
-        </a>
+        </Link>
       </nav>
     </header>
   );
