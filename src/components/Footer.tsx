7a93eed4a59db957e64d541eecef5fef6471e501
Richard Ayikwei <richardayikweibee@gmail.com>
2024-09-02 18:34:16 +0000

    removing hover to improve color contrast

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <footer className=" flex  flex-col items-center mt-9 bg-[#a3c0e2] h-[200px]">
       <div className="flex md:justify-around items-center md:w-[700px] md:h-[150px] h-[100px] md:py-6 py-20 md:flex-row flex-col bg">
-        <p className="text-white hover:text-[#1f3550]">
+        <p className="text-white ">
           <em>Email</em>: richardayikweibee@gmail.com
         </p>
-        <p className="text-white hover:text-[#1f3550]">
+        <p className="text-white ">
           <em>Contact</em>: +233557500645
         </p>
       </div>
