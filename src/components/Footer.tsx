397b0e998021ab05969e670b21340228ef8a7a39
Richard Ayikwei <richardayikweibee@gmail.com>
2024-10-18 14:03:56 +0000

    home page mobile view footer optimazation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { assets } from "../assets/assests";
 const Footer = () => {
   return (
     <footer className=" flex text-[18px] flex-col items-center mt-9 bg-[#b8a084] h-[200px]">
-      <div className="flex md:justify-around items-center md:w-[700px] md:h-[150px] h-[100px] md:py-6 py-20 md:flex-row flex-col bg">
+      <div className="flex md:justify-around items-center md:w-[700px] md:h-[150px] h-[100px] md:py-6 pt-6 md:flex-row flex-col bg">
         <p className="text-[#010101] ">
           <em>Email</em>: richardayikweibee@gmail.com
         </p>
@@ -12,12 +12,11 @@ const Footer = () => {
         </p>
       </div>
 
-      <nav className="md:w-[300px] md:h-20 flex justify-between items-center">
+      <nav className="md:w-[300px] md:h-20 flex justify-between items-center w-[200px]">
         <a
           href="https://github.com/richardayikwei"
           target="_blank"
           rel="noopener"
-          className="transition ease-linear duration-500 hover:rotate-45 pl-6"
         >
           <img
             src={assets.social.githubIcon}
@@ -29,7 +28,6 @@ const Footer = () => {
           href="https://www.linkedin.com/in/richard-ayikwei-48b171112/"
           target="_blank"
           rel="noopener"
-          className="transition ease-linear duration-500 hover:rotate-45"
         >
           <img
             src={assets.social.linkedInIcon}
@@ -41,12 +39,11 @@ const Footer = () => {
           href="https://x.com/ayikwei_richard"
           target="_blank"
           rel="noopener"
-          className="transition ease-linear duration-500 hover:rotate-45 pr-6"
         >
           <img src={assets.social.xIcon} alt="xIcon" className="w-11 h-11" />
         </a>
       </nav>
-      <div className="md:py-6 flex">
+      <div className="md:py-6 pt-3 flex text-center">
         <p className="text-[#010101]">
           &copy;<em>2024 Richard Ayikwei, All Rights Reserved.</em>
         </p>
